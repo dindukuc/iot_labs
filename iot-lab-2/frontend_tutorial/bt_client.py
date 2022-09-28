906ca53857627f2162e5ba1e94a79fadf7ab5b4f
dindukuc <32225540+dindukuc@users.noreply.github.com>
2022-09-27 19:29:07 -0700

    updated bt_client.py with raspi mac addr

--- iot-lab-2/frontend_tutorial/bt_client.py
+++ iot-lab-2/frontend_tutorial/bt_client.py
@@ -1,6 +1,6 @@
 import bluetooth
 
-host = "DC:A6:32:80:7D:87" # The address of Raspberry PI Bluetooth adapter on the server.
+host = "E4:5F:01:C6:EC:08" # The address of Raspberry PI Bluetooth adapter on the server.
 port = 1
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
